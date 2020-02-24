977d9c2193d1c9a843f0163ae988f5f3b2c18ad5
Georgeguthrie <george.guthrie98@gmail.com>
2020-02-24 18:22:01 +0000

    Includes movement, collision and two player controls

---
+++ Integrated Project 1/Assets/Scripts/Player1.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+class Player1 : MonoBehaviour
+{
+    public KeyCode moveLeftKey = KeyCode.A;
+    public KeyCode moveRightKey = KeyCode.D;
+    public KeyCode JumpKey = KeyCode.W;
+    bool canjump = false;
+    float direction = 0.0f;
+    public float speed = 0.2f;
+
+    void FixedUpdate()
+    {
+        Vector3 position = transform.localPosition;
+        position.x += speed * direction;
+        transform.localPosition = position;
+    }
+
+    void Update()
+    {
+        bool isLeftPressed = Input.GetKey(moveLeftKey);
+        bool isRightPressed = Input.GetKey(moveRightKey);
+        bool isJumpPressed = Input.GetKey(JumpKey);
+
+        if (isLeftPressed)
+        {
+            direction = -1.0f;
+        }
+        else if (isRightPressed)
+        {
+            direction = 1.0f;
+        }
+        else
+        {
+            direction = 0.0f;
+        }
+        if (isJumpPressed && canjump)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * 280);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "Bottom Wall":
+            case "Player 2":
+                canjump = true;
+                break;
+        }
+    }
+    public void OnCollisionExit2D(Collision2D other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "Bottom Wall":
+            case "Player 2":
+                canjump = false;
+                break;
+        }
+    }
+}
+
---
+++ Integrated Project 1/Assets/Scripts/Player2.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+class Player2 : MonoBehaviour
+{
+    public KeyCode moveLeftKey = KeyCode.J;
+    public KeyCode moveRightKey = KeyCode.L;
+    public KeyCode JumpKey = KeyCode.I;
+    bool canjump = false;
+    float direction = 0.0f;
+    public float speed = 0.2f;
+
+    void FixedUpdate()
+    {
+        Vector3 position = transform.localPosition;
+        position.x += speed * direction;
+        transform.localPosition = position;
+    }
+
+    void Update()
+    {
+        bool isLeftPressed = Input.GetKey(moveLeftKey);
+        bool isRightPressed = Input.GetKey(moveRightKey);
+        bool isJumpPressed = Input.GetKey(JumpKey);
+
+        if (isLeftPressed)
+        {
+            direction = -1.0f;
+        }
+        else if (isRightPressed)
+        {
+            direction = 1.0f;
+        }
+        else
+        {
+            direction = 0.0f;
+        }
+        if (isJumpPressed && canjump)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * 280);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "Bottom Wall":
+            case "Player 1":
+                canjump = true;
+                break;
+        }
+    }
+    public void OnCollisionExit2D(Collision2D other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "Bottom Wall":
+            case "Player 1":
+                canjump = false;
+                break;
+        }
+    }
+}
+
